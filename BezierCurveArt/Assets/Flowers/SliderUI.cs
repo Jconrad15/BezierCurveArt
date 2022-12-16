356cb3d98d9131ba93a6f7bd956b599b53ddb4ad
Jconrad15 <83470418+Jconrad15@users.noreply.github.com>
2022-12-16 14:47:26 -0800

    remove modular motion

--- BezierCurveArt/Assets/Flowers/SliderUI.cs
+++ BezierCurveArt/Assets/Flowers/SliderUI.cs
@@ -1,4 +1,4 @@
-using ModularMotion;
+//using ModularMotion;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,16 +8,16 @@ public class SliderUI : MonoBehaviour
     [SerializeField]
     private GameObject sliderArea;
 
-    private UIMotion uiMotion;
+    //private UIMotion uiMotion;
 
     private void Start()
     {
-        uiMotion = sliderArea.GetComponent<UIMotion>();
+        //uiMotion = sliderArea.GetComponent<UIMotion>();
     }
 
     public void Toggle()
     {
-        uiMotion.Play();
+        //uiMotion.Play();
     }
 
 }
